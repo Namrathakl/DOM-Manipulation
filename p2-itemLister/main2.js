a97dd125df47746b3fb6102b928f785986c7ab54
Namrathakl <klnamrathalokesh@gmail.com>
2023-07-25 14:37:42 +0530

    queryselector

--- p2-itemLister/main2.js
+++ p2-itemLister/main2.js
@@ -1,6 +1,20 @@
-var item = document.getElementsByClassName('list-group-item');
+/*var item = document.getElementsByClassName('list-group-item');
 item[4].style.fontWeight = 'bold';
 
 var item = document.getElementsByTagName('li');
 item[4].style.fontWeight = 'bold';
 item[4].style.backgroundColor = 'blue';
+ */
+//var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+//seconditem.style.backgroundColor = 'green'
+
+//var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+//thirditem.style.display = 'none'
+
+var item = document.querySelectorAll('.list-group-item');
+item[1].style.color = 'green'
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i< odd.length; i++){
+    odd[i].style.backgroundColor = 'green'
+}
